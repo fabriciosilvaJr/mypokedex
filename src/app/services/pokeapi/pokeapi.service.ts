deae94c782be7ae47747a09d40d9c4a8ab0355fb
Fabrício Silva <fabriciosilva187@gmail.com>
2023-10-04 21:49:25 -0300

    Método listar todos os pokedex

--- src/app/services/pokeapi/pokeapi.service.ts
+++ src/app/services/pokeapi/pokeapi.service.ts
@@ -8,4 +8,20 @@ import { HttpClient } from '@angular/common/http';
 export class PokeapiService {
 
   constructor(private http: HttpClient) { }
+
+  getPokedex() {
+
+    return new Promise((resolve, reject) => {
+
+      this.http.get("https://pokeapi.co/api/v2/pokedex/2").subscribe((data: any) => {
+        return resolve(data);
+      }, (err: any) => {
+        return reject(err);
+      })
+
+    });
+
+  }
+
+
 }
