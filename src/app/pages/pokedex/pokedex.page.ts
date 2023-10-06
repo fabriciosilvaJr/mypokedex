20ea2c208a7f2c4f9bafda827b3331448aa6e70f
Fabrício Silva <fabriciosilva187@gmail.com>
2023-10-06 19:42:08 -0300

    Adicionando paginação scroll em pokedex

--- src/app/pages/pokedex/pokedex.page.ts
+++ src/app/pages/pokedex/pokedex.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { PokeapiService } from 'src/app/services/pokeapi/pokeapi.service';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-pokedex',
@@ -10,19 +11,45 @@ import { PokeapiService } from 'src/app/services/pokeapi/pokeapi.service';
 export class PokedexPage implements OnInit {
   pokedex: any = [];
   query: string = '';
+  currentPage: number = 1; // Página atual
+  itemsPerPage: number = 10; // Itens por página
+  isInfiniteScrollDisabled: boolean = false;
+  @ViewChild(IonInfiniteScroll) infiniteScroll!: IonInfiniteScroll;
+
   constructor(public pokeapiService: PokeapiService, public router:Router) { 
-    this.getPokedex();
+    // this.getPokedex();
   }
 
-  getPokedex() {
+  // getPokedex() {
 
-    this.pokeapiService.getPokedex().then((data: any) => {
-      this.pokedex = data.pokemon_entries;
-    });
+  //   this.pokeapiService.getPokedex().then((data: any) => {
+  //     this.pokedex = data.pokemon_entries;
+  //   });
 
-  }
+  // }
 
   ngOnInit() {
+    this.loadData();
+
+  }
+  loadData(event?: any) {
+    this.pokeapiService.getPokedex(this.currentPage, this.itemsPerPage).then((data: any) => {
+      const newPokemonData = data;
+
+      // Verificar se há mais itens a serem carregados
+      if (newPokemonData.length > 0) {
+        this.pokedex = [...this.pokedex, ...newPokemonData];
+        this.currentPage++;
+      } else {
+        // Se não houver mais itens, desative o carregamento infinito
+        this.isInfiniteScrollDisabled = true;
+      }
+
+      // Encerrar o evento do IonInfiniteScroll
+      if (event) {
+        event.target.complete();
+      }
+    });
   }
 
   verMais(pokemonId: string) {
